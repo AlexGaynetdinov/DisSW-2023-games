b7672324f9b62ddfde7dbbf664dc628c800d1ae5
AluIsmael <114938688+AluIsmael@users.noreply.github.com>
2023-04-14 20:41:12 +0200

    Add files via upload
    Entidad Token implementada en clase

---
+++ games/src/main/java/edu/esi/uclm/es/ds/games/entities/Token.java
@@ -0,0 +1,48 @@
+package edu.uclm.esi.ds.games.domain;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Token {
+	@Id
+	@Column(length = 36)
+	private String id;
+	private long creationTime;
+	private long confirmationtime;
+	@ManyToOne
+	private User user;
+	
+	public Token() {
+		this.id = UUID.randomUUID().toString();
+		this.creationTime = System.currentTimeMillis();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public long getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(long creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public long getConfirmationtime() {
+		return confirmationtime;
+	}
+
+	public void setConfirmationtime(long confirmationtime) {
+		this.confirmationtime = confirmationtime;
+	}
+}
