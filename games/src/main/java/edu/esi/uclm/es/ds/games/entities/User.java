c1f0df4cddb527dc5a908cc29547c59a1188bf43
AluIsmael <114938688+AluIsmael@users.noreply.github.com>
2023-06-24 12:50:23 +0200

    Update User.java

--- games/src/main/java/edu/esi/uclm/es/ds/games/entities/User.java
+++ games/src/main/java/edu/esi/uclm/es/ds/games/entities/User.java
@@ -10,57 +10,60 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
-@Entity @Table(name = "users", 
-			indexes = {
-					@Index(columnList = "name", unique = true),
-					@Index(columnList = "email", unique = true)
-					}
-			)
-
+@Entity
+@Table(
+		name = "users",
+		indexes = {
+				@Index(columnList = "name", unique = true),
+				@Index(columnList = "email", unique = true),
+		}
+)
 public class User {
 	@Id @Column(length = 36)
-	private String  id;
+	private String id;
 	@Column(length = 100) @NotEmpty
 	private String name;
 	@Column(length = 140) @NotEmpty
 	private String email;
-	@NotEmpty
+	@Column(length = 255) @NotEmpty
 	private String pwd;
+	@Column(length = 8) @NotEmpty
+	private Integer partidas;
 	
 	public User() {
-		this.id = UUID.randomUUID().toString();
+		id = UUID.randomUUID().toString();
+		partidas = 0;
 	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getEmail() {
-		return email;
-	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	// The password must be encrypted when storing
 	public void setPwd(String pwd) {
 		this.pwd = org.apache.commons.codec.digest.DigestUtils.sha512Hex(pwd);
+	}
+	public void setPartidas(int partidas) {
+		this.partidas = partidas;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	public String getPwd() {
+		return this.pwd;
+	}
+	public int getPartidas() {
+		return this.partidas;
+	}
+	public String getId() {
+		return this.id;
 	}
 }
