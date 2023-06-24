98fffe8f062ccb5501782387a4c02a0210c6dafe
AluIsmael <114938688+AluIsmael@users.noreply.github.com>
2023-06-24 12:49:17 +0200

    Update UsersController.java

--- games/src/main/java/edu/esi/uclm/es/ds/games/http/UsersController.java
+++ games/src/main/java/edu/esi/uclm/es/ds/games/http/UsersController.java
@@ -1,26 +1,26 @@
 package edu.esi.uclm.es.ds.games.http;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import edu.esi.uclm.es.ds.games.domain.Match;
-import edu.esi.uclm.es.ds.games.services.GamesService;
-import edu.esi.uclm.es.ds.games.services.UsersService;
+import edu.uclm.esi.ds.games.domain.User;
+import edu.uclm.esi.ds.games.services.UsersService;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("users")
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 class UsersController {
 	
 	@Autowired
