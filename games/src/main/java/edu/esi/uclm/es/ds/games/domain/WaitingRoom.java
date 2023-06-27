c5d968647ecc220274807b222a436db26a67bd52
AluIsmael <114938688+AluIsmael@users.noreply.github.com>
2023-06-27 14:41:49 +0200

    Update WaitingRoom.java

--- games/src/main/java/edu/esi/uclm/es/ds/games/domain/WaitingRoom.java
+++ games/src/main/java/edu/esi/uclm/es/ds/games/domain/WaitingRoom.java
@@ -1,28 +1,59 @@
 package edu.esi.uclm.es.ds.games.domain;
 
+import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
 
-import edu.esi.uclm.es.ds.games.entities.User;
-
 public class WaitingRoom {
+	// notYetFullMatches<idMatch, Match>
+	// Partidas con un solo jugador (en espera)
+	private ConcurrentHashMap<String, Match> notYetFullMatches;
 	
-	private ConcurrentHashMap<String, Match> matches;
-	
+	// matches<idMatch, Match>
+	// Partidas que están llenas
+	private ConcurrentHashMap<String, Match> fullMatches;
+
 	public WaitingRoom() {
-		this.matches = new ConcurrentHashMap<>();
+		this.fullMatches = new ConcurrentHashMap<>();
+		this.notYetFullMatches = new ConcurrentHashMap<>();
 	}
 
-	public Match findMatch(String juego, User user) {
-		Match match = this.matches.get(juego);
-		if (match==null) {
-			match = new Match();
-			match.addPlayer(user);
-			this.matches.put(juego, match);
-		}else {
-			match.addPlayer(user);
+	public Match findMatch(String playerToken) {
+		// Esta persona no esta en ninguna partida, tenemos que encontrarle una o crear una nueva
+		Enumeration<String> games = notYetFullMatches.keys();
+		try {
+			// Intentamos tomar una partida de un jugador
+			while (true) { // Si no hay ninguna salta una excepcion
+				// Sacamos el id de la partida
+				String gameId = games.nextElement();
+				
+				if (gameId.equals("") || gameId == null) {
+					throw new Exception();
+				}
+				
+				// Sacamos la partida con el id
+				Match match = notYetFullMatches.get(gameId);
+				
+				// Comprobamos si el jugador que hay en la partida soy yo
+				if (match.getPlayers().contains(playerToken)) {
+					continue;
+				}
+				
+				match.addPlayer(playerToken);
+				
+				// Como la partida ya existía entonces había algun jugador dentro, por lo que ahora esta llena
+				this.fullMatches.put(gameId, match);
+				this.notYetFullMatches.remove(gameId);
+
+				return match;
+			}
+		} catch(Exception e) {
+			// Si no hay partidas con un solo jugador se crea una y se añade al jugador
+			Match match = new Match();
+			match.addPlayer(playerToken);
+			// Como acaba de ser creada, se añade a las partidas no llenas
+			this.notYetFullMatches.put(match.getId(), match);
+
+			return match;
 		}
-		
-		return match;
 	}
-
 }
