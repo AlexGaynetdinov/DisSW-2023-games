e905b4204d3a99807653f9c26807986e6be2c08b
AluIsmael <114938688+AluIsmael@users.noreply.github.com>
2023-06-27 15:20:30 +0200

    Update GameController.java

--- games/src/main/java/edu/esi/uclm/es/ds/games/http/GameController.java
+++ games/src/main/java/edu/esi/uclm/es/ds/games/http/GameController.java
@@ -1,33 +1,34 @@
 package edu.esi.uclm.es.ds.games.http;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import edu.uclm.esi.ds.games.domain.Match;
+import edu.uclm.esi.ds.games.services.GamesService;
+import edu.uclm.esi.ds.games.services.UsersService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import edu.esi.uclm.es.ds.games.dao.UserDAO;
-import edu.esi.uclm.es.ds.games.domain.Match;
-import edu.esi.uclm.es.ds.games.entities.User;
-import edu.esi.uclm.es.ds.games.services.GamesService;
 
 @RestController
 @RequestMapping("games")
-@CrossOrigin(origins = "*")
-class GameController {
-	@Autowired
-	UserDAO userDAO;
+@CrossOrigin("*")
+public class GamesController {
+
 	@Autowired
 	private GamesService gamesService;
+	@Autowired
+	private UsersService userService;
+
 	@GetMapping("/requestGame")
-	public Match requestGame(@RequestParam String juego, @RequestParam String player) {
-		if (!juego.equals("nm"))
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado el juego");
-		User user = userDAO.findByName(player);
-		return this.gamesService.requestGame(juego, user);
-		
+	public Match request_game(@RequestParam String player) {
+		if (userService.getUser(player) == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El usuario no ha iniciado sesión.");
+		}
+		return this.gamesService.request_game(player);
 	}
 }
